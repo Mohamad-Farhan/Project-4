117e435ecd12866debda426a06f91ab6c8bebe7d
tetra-shayeb <mohammednm40@gmail.com>
2020-11-19 21:37:16 +0200

    Fixed some err

--- Frontend/src/Components/ContactUs/Contact.js
+++ Frontend/src/Components/ContactUs/Contact.js
@@ -1,19 +1,20 @@
+import React from 'react'
 import axios from 'axios'
 import './Contact.css'
 import AddIcCallIcon from '@material-ui/icons/AddIcCall';
 import EmailIcon from '@material-ui/icons/Email';
-import {useState} from 'react'
-import Navbar from './Navbar'
-
 import { useState } from 'react'
 import Navbar from './Navbar'
 
+
 function Contact() {
 
-    const { FullName, setFullName } = useState("");
-    const { PhoneNumber, setPhoneNumber } = useState(0);
-    const { Email, setEmail } = useState("");
-    const { Message, setMessage } = useState("");
+    const [FullName, setFullName] = useState("");
+    const [PhoneNumber, setPhoneNumber] = useState(0);
+    const [Email, setEmail] = useState("");
+    const [Message, setMessage] = useState("");
+
+
 
     const addMessage = () => {
         axios.post("http://localhost:3000/contact-us",
@@ -26,75 +27,53 @@ function Contact() {
     }
     return (
 
-
-
-
-       function Contact() {
-
-           const [FullName , setFullName]= useState("");
-           const [PhoneNumber , setPhoneNumber] = useState(0);
-           const [Email , setEmail] = useState("");
-           const [Message , setMessage] = useState("");
-       
-
-
-           const addMessage=()=>{
-               axios.post("http://localhost:3000/contact-us" ,
-                {Full_Name:FullName , Phone_Number:PhoneNumber , Email:Email  , Message:Message}).then((responce)=>{
-                 console.log('sucessfully')
-                })
-                .catch((err)=>{
-                    console.log('Err', err)
-                })
-           }
-    return (
-          
-        <div> 
-            <Navbar/>
-           <div className='field'> <h1 className='header'> Contact Us </h1></div>
-             <div classNam='codeeam'>
-             <div >
-                 <h4> Codeeam comunity team welcomes you in our website</h4>
-                 <p> and we wish you a pleasant time </p>
-                 <p> We are happy to Contact Us.</p>
-             </div>
-             </div>
-             
-             <div className='information'>  
-                 <h3>  Contact Information </h3>
-                 <p > <AddIcCallIcon/> Phone Number : +962-786025324 </p>
-                 <p> <EmailIcon/> Email : Codeeam@gmail.com</p>
-             </div>
-
         <div>
             <Navbar />
-            <div> <h1 className='header'> Contact Us </h1></div>
-            <div className='contact-form'>
-                <h3> Get in touch</h3>
-                <div className='text'>
-                    <label> Full Name </label> <input type='text' placeholder='Enter your Full Name ' onChange={(event) => {
-                        setFullName(event.target.value)
+            <div className='field'> <h1 className='header'> Contact Us </h1></div>
+            <div classNam='codeeam'>
+                <div >
+                    <h4> Codeeam comunity team welcomes you in our website</h4>
+                    <p> and we wish you a pleasant time </p>
+                    <p> We are happy to Contact Us.</p>
+                </div>
+            </div>
+
+            <div className='information'>
+                <h3>  Contact Information </h3>
+                <p > <AddIcCallIcon /> Phone Number : +962-786025324 </p>
+                <p> <EmailIcon /> Email : Codeeam@gmail.com</p>
+            </div>
+
+            <div>
+                
+                
+                <div className='contact-form'>
+                    <h3> Get in touch</h3>
+                    <div className='text'>
+                        <label> Full Name </label> <input type='text' placeholder='Enter your Full Name ' onChange={(event) => {
+                            setFullName(event.target.value)
+                        }} />
+                    </div>
+
+                    <div className='text'> <label> Phone Number</label> <input type='number' placeholder='Enter your phone number' onChange={(event) => {
+                        setPhoneNumber(event.target.value)
                     }} />
+                    </div>
+                    <div className='text'>
+                        <label>Email</label><input type='text' placeholder='Enter your email ' onChange={(event) => {
+                            setEmail(event.target.value)
+                        }} />
+                    </div>
+                    <div className='text'>
+                        <label>Message</label>  <textarea onChange={(event) => {
+                            setMessage(event.target.value)
+                        }} />
+                    </div>
+                    <button onClick={addMessage}> SEND </button>
                 </div>
-                <div className='text'> <label> Phone Number</label> <input type='number' placeholder='Enter your phone number' onChange={(event) => {
-                    setFullName(event.target.value)
-                }} />
-                <div className='text'> <label> Phone Number</label> <input type='number' placeholder='Enter your phone number' onChange={(event)=>{
-                    setPhoneNumber(event.target.value)}}/> 
-                </div>
-                <div className='text'>
-                    <label>Email</label><input type='text' placeholder='Enter your email ' onChange={(event) => {
-                        setEmail(event.target.value)
-                    }} />
-                </div>
-                <div className='text'>
-                    <label>Message</label>  <textarea onChange={(event) => {
-                        setMessage(event.target.value)
-                    }} />
-                </div>
-                <button onClick={addMessage}> SEND </button>
             </div>
         </div>
+
     )
 }
-export default Contact;
+export default Contact
